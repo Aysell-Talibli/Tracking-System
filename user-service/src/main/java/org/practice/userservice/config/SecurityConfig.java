cdf7382079fc6128967d21d2b6fe79978419d520
Aysell-Talibli <a.talibli.01@gmail.com>
2025-06-24 15:48:26 +0400

    Permissions changed

--- user-service/src/main/java/org/practice/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/org/practice/userservice/config/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login", "/auth/**").permitAll() // ✅ allow public access
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
